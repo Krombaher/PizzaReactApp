369267efb3a6a43dc8e8c3a35ab45a8033395bb3
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-08-31 23:16:05 +0300

    sort pizza

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import Pizza from './components/Pizza';
+import Sort from './components/Sort';
+import './scss/app.scss';
+import pizzas from './assets/pizzas.json';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {
+                pizzas.map((obj) => (
+                  <Pizza key={obj.id} {...obj}/>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
